2ff894a7ac39727200c6c55c0bfc6d82ee56abb0
shreyapohekar <shreyapohekar@gmail.com>
2022-12-18 19:25:34 +0530

    Create pop.php

---
+++ pop.php
@@ -0,0 +1,21 @@
+<?php 
+
+$activeTab = $_GET['tab'];
+
+echo intval($activeTab);
+
+$car = $_POST['asd'];
+echo $car;
+
+
+
+$car3 = $_POST['asd'];
+echo "Abc";
+echo $car3;
+
+
+$car4 = absint($_POST['asd']);
+echo $car;
+
+
+?>
